c893e1731fce3f1049921ad53daac752ae109d5d
Ramon <ramonmaito@gmail.com>
2021-08-20 14:56:36 -0300

    problemas no autor

--- src/main/java/br/edu/ifsul/dao/LivroDAO.java
+++ src/main/java/br/edu/ifsul/dao/LivroDAO.java
@@ -5,6 +5,7 @@
  */
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.converters.ConverterOrdem;
 import br.edu.ifsul.modelo04.Livro;
 import java.io.Serializable;
 import javax.ejb.Stateful;
@@ -19,6 +20,14 @@ public class LivroDAO<TIPO> extends DAOGenerico<Livro> implements Serializable {
     public LivroDAO() {
         super();
         classePersistente = Livro.class;
+        listaOrdem.add(new Ordem("isbn", "ISBN", "="));
+        listaOrdem.add(new Ordem("titulo", "Titulo", "like"));
+        // definição da ordem atual
+        ordemAtual = listaOrdem.get(1); // vai pegar o segundo da lista de ordens
+        // criando uma instancia do conversor
+        converterOrdem = new ConverterOrdem();
+        // associando a lista de ordens ao conversor
+        converterOrdem.setListaOrdem(listaOrdem);
     }
 
 }
